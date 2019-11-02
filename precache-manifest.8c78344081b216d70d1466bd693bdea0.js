289170cbf497bf546f297b95970db004307fcc40
Angelica Zamer <angelica.zamer@gmail.com>
2019-11-01 23:30:18 -0500

    Updates

---
+++ precache-manifest.8c78344081b216d70d1466bd693bdea0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f444c8e5750e1f5c24dc38e44be615e",
+    "url": "/TheSimpsons/index.html"
+  },
+  {
+    "revision": "18cb8e33c47ffa53321e",
+    "url": "/TheSimpsons/static/css/main.d86b1a21.chunk.css"
+  },
+  {
+    "revision": "9140848708ab0cf7f0f9",
+    "url": "/TheSimpsons/static/js/2.0d9a0da0.chunk.js"
+  },
+  {
+    "revision": "18cb8e33c47ffa53321e",
+    "url": "/TheSimpsons/static/js/main.8b773ff7.chunk.js"
+  },
+  {
+    "revision": "d0cde9a9d2addf63b77b",
+    "url": "/TheSimpsons/static/js/runtime-main.883d4026.js"
+  }
+]);
